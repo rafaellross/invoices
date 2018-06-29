c0f40d6ce4b4ca2f97c37c549920740beb3cdabf
Rafael Ross <rafaellross@gmail.com>
2018-06-29 20:55:21 +1000

    first commit

---
+++ src/views/TableList/TableList.jsx
@@ -0,0 +1,97 @@
+import React, {Component} from 'react';
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+import Grid from "@material-ui/core/Grid";
+// core components
+import GridItem from "components/Grid/GridItem.jsx";
+import Table from "components/Table/Table.jsx";
+import Card from "components/Card/Card.jsx";
+import CardHeader from "components/Card/CardHeader.jsx";
+import CardBody from "components/Card/CardBody.jsx";
+import axios from 'axios';
+
+const styles = {
+  cardCategoryWhite: {
+    "&,& a,& a:hover,& a:focus": {
+      color: "rgba(255,255,255,.62)",
+      margin: "0",
+      fontSize: "14px",
+      marginTop: "0",
+      marginBottom: "0"
+    },
+    "& a,& a:hover,& a:focus": {
+      color: "#FFFFFF"
+    }
+  },
+  cardTitleWhite: {
+    color: "#FFFFFF",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: "3px",
+    textDecoration: "none",
+    "& small": {
+      color: "#777",
+      fontSize: "65%",
+      fontWeight: "400",
+      lineHeight: "1"
+    }
+  }
+};
+
+
+
+class TableList extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {value: '', items: ''};
+      const { classes } = styles;
+  }
+
+
+  componentDidMount(){
+    axios.get('http://localhost:8000/items')
+    .then(response => {
+      this.setState({ items: response.data });
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+
+  }
+
+render(){
+  return (
+    <Grid container>
+      <GridItem xs={12} sm={12} md={12}>
+        <Card>
+          <CardHeader color="primary">
+            <h4 className={styles.cardTitleWhite}>Simple Table</h4>
+            <p className={styles.cardCategoryWhite}>
+              Here is a subtitle for this table
+            </p>
+          </CardHeader>
+          <CardBody>
+
+          {this.state.items !== ''  ? (
+            <Table
+              tableHeaderColor="primary"
+              tableHead={["#", "Name", "Price"]}
+              tableData={this.state.items}
+            />
+
+          ) : (
+            <div>Loading</div>
+          )}
+
+
+          </CardBody>
+        </Card>
+      </GridItem>
+    </Grid>
+  );
+}
+}
+
+export default withStyles(styles)(TableList);
